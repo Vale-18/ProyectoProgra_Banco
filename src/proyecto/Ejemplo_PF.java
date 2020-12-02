883cc4762a9752db482146357a4f31966335274b
emanuel-bg <72946244+emanuel-bg@users.noreply.github.com>
2020-12-02 15:09:04 -0600

    CLASE MAIN

---
+++ src/proyecto/Ejemplo_PF.java
@@ -0,0 +1,17 @@
+
+package proyecto;
+
+import proyecto.Menu;
+
+public class Ejemplo_PF 
+{
+
+    public static void main(String[] args) 
+    {
+   //hbjklasrfghjnipasdfvbjnikasrgkbjni
+     Menu menu = new Menu();
+     menu.menu();  
+
+    }
+    
+}
