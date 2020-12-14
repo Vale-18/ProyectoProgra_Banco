4e0a40275bd4b5c2eb9a14608a795ba1d2941023
Valeria <Gretell@DESKTOP-I579FDB>
2020-12-13 20:42:55 -0600

    Eliminación de comentarios

--- src/proyecto/Ejemplo_PF.java
+++ src/proyecto/Ejemplo_PF.java
@@ -1,14 +1,11 @@
 
 package proyecto;
 
-import proyecto.Menu;
-
 public class Ejemplo_PF 
 {
 
     public static void main(String[] args) 
     {
-   //hbjklasrfghjnipasdfvbjnikasrgkbjni
      Menu menu = new Menu();
      menu.menu();  
 
